8a9917173857d137a0e4246d53e3c5b8a703b92f
morozov <morozov@surfstudio.ru>
2021-01-18 16:02:24 +0300

    show alert on press button

--- AvitoInternship/AvitoInternship/ViewController.swift
+++ AvitoInternship/AvitoInternship/ViewController.swift
@@ -12,9 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonPressed() {
+        let alert = UIAlertController(title: "Выбрана услуга", message: nil, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
