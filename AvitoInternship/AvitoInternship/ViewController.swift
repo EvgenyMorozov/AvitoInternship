a04ebbbe32ef98b25dac3262629a4d7506d391e3
morozov <morozov@surfstudio.ru>
2021-01-18 04:14:40 +0300

    create project

---
+++ AvitoInternship/AvitoInternship/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AvitoInternship
+//
+//  Created by Evgeny Morozov on 18/01/2021.
+//  Copyright © 2021 EvgenyMorozov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
